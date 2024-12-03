e87e5af2f4e5275e5df9d91fcb9af285712b019d
Walther <email+git@waltteri.net>
2024-12-03 14:13:33 +0200

    feat: add solution for day 3 part 2

--- Justfile
+++ Justfile
@@ -6,7 +6,9 @@ default:
 @day DAY:
   echo "Advent of Code Day {{DAY}}"
   cargo run --quiet --release --bin day-$(printf "%02d" {{DAY}})
+alias d := day
 
 # Run all tests
 @test:
   cargo nextest run --cargo-quiet
+alias t := test
--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -11,36 +11,84 @@ fn main() {
     println!("Part 2: {value}");
 }
 
-type ParsedData = Vec<(u32, u32)>;
+type ParsedData = Vec<Instruction>;
+enum Instruction {
+    Mul(u32, u32),
+    Do,
+    Dont,
+}
 
 fn parse(input: &str) -> ParsedData {
-    let mut muls = Vec::new();
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").expect("Failed to construct a regular expression");
-    for (_, [left, right]) in re.captures_iter(input).map(|c| c.extract()) {
-        let l: u32 = left.parse().expect("Unable to parse number");
-        let r: u32 = right.parse().expect("Unable to parse number");
-        muls.push((l, r));
+    let mut instructions: Vec<Instruction> = Vec::new();
+    let instruction_regex = Regex::new(r"(mul\(\d+,\d+\))|(do\(\))|(don't\(\))")
+        .expect("Failed to construct a regular expression");
+    let mul_regex =
+        Regex::new(r"mul\((\d+),(\d+)\)").expect("Failed to construct a regular expression");
+
+    for (_full, [instruction]) in instruction_regex.captures_iter(input).map(|c| c.extract()) {
+        match instruction {
+            "do()" => {
+                instructions.push(Instruction::Do);
+                continue;
+            }
+            "don't()" => {
+                instructions.push(Instruction::Dont);
+                continue;
+            }
+            _ => {
+                let (_full, [mul_left, mul_right]) = mul_regex
+                    .captures(instruction)
+                    .expect("Unable to parse mul instruction")
+                    .extract();
+                let l: u32 = mul_left.parse().expect("Unable to parse number");
+                let r: u32 = mul_right.parse().expect("Unable to parse number");
+                instructions.push(Instruction::Mul(l, r));
+            }
+        }
     }
 
-    muls
+    instructions
 }
 
 fn method_1(data: &ParsedData) -> u32 {
-    data.iter().map(|(l, r)| l * r).sum()
+    let mut total = 0;
+    for instruction in data {
+        match instruction {
+            Instruction::Mul(l, r) => total += l * r,
+            _ => continue,
+        }
+    }
+    total
 }
 
 fn method_2(data: &ParsedData) -> u32 {
-    2
+    let mut total = 0;
+    let mut enabled = true;
+    for instruction in data {
+        match instruction {
+            Instruction::Mul(l, r) => {
+                if enabled {
+                    total += l * r;
+                }
+            }
+            Instruction::Do => enabled = true,
+            Instruction::Dont => enabled = false,
+        }
+    }
+    total
 }
 
 #[cfg(test)]
 mod tests {
     use crate::*;
-    const INPUT: &str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT_1: &str =
+        r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT_2: &str =
+        r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part1() {
-        let parsed = parse(INPUT);
+        let parsed = parse(INPUT_1);
         let value = method_1(&parsed);
         let expected = 161;
         assert_eq!(value, expected);
@@ -48,9 +96,9 @@ mod tests {
 
     #[test]
     fn part2() {
-        let parsed = parse(INPUT);
+        let parsed = parse(INPUT_2);
         let value = method_2(&parsed);
-        let expected = 2;
+        let expected = 48;
         assert_eq!(value, expected);
     }
 }
