34963a6c4e65adc4152c4061ebb6f10a7e3c0b70
Walther <email+git@waltteri.net>
2024-12-03 13:28:36 +0200

    feat: add solution for day 3 part 1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 fn main() {
     const INPUT: &str = include_str!("input.txt");
     let parsed = parse(INPUT);
@@ -9,12 +11,22 @@ fn main() {
     println!("Part 2: {value}");
 }
 
-type ParsedData = Vec<()>;
+type ParsedData = Vec<(u32, u32)>;
 
-fn parse(input: &str) -> ParsedData {}
+fn parse(input: &str) -> ParsedData {
+    let mut muls = Vec::new();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").expect("Failed to construct a regular expression");
+    for (_, [left, right]) in re.captures_iter(input).map(|c| c.extract()) {
+        let l: u32 = left.parse().expect("Unable to parse number");
+        let r: u32 = right.parse().expect("Unable to parse number");
+        muls.push((l, r));
+    }
+
+    muls
+}
 
 fn method_1(data: &ParsedData) -> u32 {
-    1
+    data.iter().map(|(l, r)| l * r).sum()
 }
 
 fn method_2(data: &ParsedData) -> u32 {
@@ -24,13 +36,13 @@ fn method_2(data: &ParsedData) -> u32 {
 #[cfg(test)]
 mod tests {
     use crate::*;
-    const INPUT: &str = r"";
+    const INPUT: &str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     #[test]
     fn part1() {
         let parsed = parse(INPUT);
         let value = method_1(&parsed);
-        let expected = 1;
+        let expected = 161;
         assert_eq!(value, expected);
     }
 
