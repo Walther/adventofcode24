6607db401a78c5760fb0fce9accc36c3b9fbf9e5
Walther <email+git@waltteri.net>
2024-12-04 15:10:09 +0200

    feat: add solution for day 4 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -72,44 +72,66 @@ impl<'a> Visitor<'a> {
         Self { maze, x, y }
     }
 
-    fn step(&mut self, direction: Direction) -> Option<&char> {
+    fn get(&self) -> Option<&char> {
+        self.maze.get(self.x, self.y)
+    }
+
+    fn coordinate_in_direction(&self, direction: Direction) -> Option<(usize, usize)> {
+        let x;
+        let y;
         match direction {
             Direction::NW => {
-                self.x = self.x.checked_sub(1)?;
-                self.y = self.y.checked_sub(1)?;
+                x = self.x.checked_sub(1)?;
+                y = self.y.checked_sub(1)?;
             }
             Direction::N => {
-                self.y = self.y.checked_sub(1)?;
+                x = self.x;
+                y = self.y.checked_sub(1)?;
             }
             Direction::NE => {
-                self.x = self.x.checked_add(1)?;
-                self.y = self.y.checked_sub(1)?;
+                x = self.x.checked_add(1)?;
+                y = self.y.checked_sub(1)?;
             }
             Direction::W => {
-                self.x = self.x.checked_sub(1)?;
+                x = self.x.checked_sub(1)?;
+                y = self.y;
             }
             Direction::E => {
-                self.x = self.x.checked_add(1)?;
+                x = self.x.checked_add(1)?;
+                y = self.y;
             }
             Direction::SW => {
-                self.x = self.x.checked_sub(1)?;
-                self.y = self.y.checked_add(1)?;
+                x = self.x.checked_sub(1)?;
+                y = self.y.checked_add(1)?;
             }
             Direction::S => {
-                self.y = self.y.checked_add(1)?;
+                x = self.x;
+                y = self.y.checked_add(1)?;
             }
             Direction::SE => {
-                self.x = self.x.checked_add(1)?;
-                self.y = self.y.checked_add(1)?;
+                x = self.x.checked_add(1)?;
+                y = self.y.checked_add(1)?;
             }
         }
 
-        self.maze.get(self.x, self.y)
+        Some((x, y))
+    }
+
+    fn peek(&self, direction: Direction) -> Option<&char> {
+        let (x, y) = self.coordinate_in_direction(direction)?;
+        self.maze.get(x, y)
+    }
+
+    fn step(&mut self, direction: Direction) -> Option<&char> {
+        let (x, y) = self.coordinate_in_direction(direction)?;
+        self.x = x;
+        self.y = y;
+        self.get()
     }
 
     fn collect(&mut self, max_length: usize, direction: Direction) -> Option<String> {
         let mut collection = String::new();
-        let grab = self.maze.get(self.x, self.y)?;
+        let grab = self.get()?;
         collection.push(*grab);
 
         while collection.len() < max_length {
@@ -120,6 +142,20 @@ impl<'a> Visitor<'a> {
         }
 
         Some(collection)
+    }
+
+    fn surroundings(&self) -> Option<[&char; 9]> {
+        Some([
+            self.peek(NW)?,
+            self.peek(N)?,
+            self.peek(NE)?,
+            self.peek(W)?,
+            self.get()?,
+            self.peek(E)?,
+            self.peek(SW)?,
+            self.peek(S)?,
+            self.peek(SE)?,
+        ])
     }
 }
 
@@ -144,8 +180,44 @@ fn part1(data: &ParsedData) -> usize {
     xmas_count
 }
 
-fn part2(_data: &ParsedData) -> usize {
-    2
+fn part2(data: &ParsedData) -> usize {
+    let mut x_max_count = 0;
+    for &(x, y) in data.maze.keys() {
+        let visitor = Visitor::new(data, x, y);
+        let surroundings = visitor.surroundings();
+        match surroundings {
+            Some(map) => {
+                if is_x_mas(map) {
+                    x_max_count += 1;
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    x_max_count
+}
+
+#[rustfmt::skip]
+fn is_x_mas(map: [&char; 9]) -> bool {
+    matches!(map,
+        [
+            'M', _, 'M',
+            _,  'A',  _,
+            'S', _, 'S',
+        ] | [
+            'S', _, 'M',
+            _,  'A',  _,
+            'S', _, 'M',
+        ] | [
+            'M', _, 'S',
+            _,  'A',  _,
+            'M', _, 'S',
+        ] | [
+            'S', _, 'S',
+            _,  'A',  _,
+            'M', _, 'M',
+        ])
 }
 
 #[cfg(test)]
@@ -173,7 +245,7 @@ MXMXAXMASX";
     fn part2() {
         let parsed = crate::parse(INPUT);
         let value = crate::part2(&parsed);
-        let expected = 2;
+        let expected = 9;
         assert_eq!(value, expected);
     }
 }
